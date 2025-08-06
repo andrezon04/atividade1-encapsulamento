849b819ec10659d564178fc839d407ded86bc36e
André Dias <diasa0421@gmail.com>
2025-08-06 15:36:05 -0300

    Update Main.java

--- ValidacaoIdade/Main.java
+++ ValidacaoIdade/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        pessoa.setNome("João Silva");
-        pessoa.setIdade(-30);
+        pessoa.setNome("André Dias");
+        pessoa.setIdade(20);
 
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Idade: " + pessoa.getIdade());
     }
+
 }
